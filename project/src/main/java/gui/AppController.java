cb35edb098d7527259604df78e30d681425f38ee
Martin Skatvedt <martinskatvedt@gmail.com>
2021-04-14 21:41:09 +0200

    more ui elements and save file

--- project/src/main/java/gui/AppController.java
+++ project/src/main/java/gui/AppController.java
@@ -1,5 +1,10 @@
 package gui;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import dataHandler.Cell;
 import dataHandler.Grid;
 import javafx.animation.Animation.Status;
 import javafx.animation.KeyFrame;
@@ -9,13 +14,15 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
 
 public class AppController {
 	private int gridSize = 50;
 	double canvasSize;
-	double animationSpeed = 0.5;
+	double animationSpeed = 0.1;
+	private double resolution;
 	
 	Timeline timeline;
 	Grid grid;
@@ -25,9 +32,12 @@ public class AppController {
 	@FXML private Button slowButton;
 	@FXML private Button normalButton;
 	@FXML private Button fastButton;
+	@FXML private Button smallGridButton;
+	@FXML private Button mediumGridButton;
+	@FXML private Button bigGridButton;
 
 	public AppController() {
-		this.grid = new Grid(this.gridSize, true);
+		this.grid = new Grid(this.gridSize, false);
 		
 	    this.timeline = new Timeline(new KeyFrame(Duration.seconds(this.animationSpeed), e -> this.nextFrame()));
 	    this.timeline.setCycleCount(Timeline.INDEFINITE);
@@ -36,10 +46,48 @@ public class AppController {
 	@FXML
 	private void initialize() {
 		this.canvasSize = this.canvas.getHeight();
+		this.resolution = canvasSize/this.gridSize;
+		this.fillGrid();
+		
+		this.canvas.setOnMouseDragged((MouseEvent event) -> {
+	        this.handleMouseEvent(event);
+	    });
+		
+		this.canvas.setOnMouseClicked((MouseEvent event) -> {
+			this.handleMouseEvent(event);
+	    });
+		
+	}
+	
+	public void loadGrid() {
+		
+	}
+	
+	public void saveGrid() throws IOException {
+		BufferedWriter br = new BufferedWriter(new FileWriter("myfile.csv"));
+		StringBuilder sb = new StringBuilder();
+		for (Cell[] row : this.grid.getGrid()) {
+			for (Cell cell : row) {
+				sb.append(cell);
+				sb.append(",");
+			}
+			br.write(sb.toString());
+
+		}
+
+		br.close();
+		
+		System.out.println("Saved");
+	}
+	
+	public void clearCanvas() {
+		if (this.timeline.getStatus() == Status.RUNNING) this.startSimulation();
+		this.grid = new Grid(this.gridSize, false);
+		this.canvasSize = this.canvas.getHeight();
+		this.resolution = canvasSize/this.gridSize;
 		this.fillGrid();
 	}
 	
-	@FXML
 	public void startSimulation() {
 		if (this.timeline.getStatus() == Status.RUNNING) {
 			this.timeline.stop();
@@ -51,6 +99,41 @@ public class AppController {
 		}
 	}
 	
+	public void changeGridSize(ActionEvent event) {
+		int newGridSize = 25;
+		Button button = (Button) event.getSource();
+		switch (button.getText()) {
+		case "50x50":
+			newGridSize = 50;
+			break;
+		case "25x25":
+			newGridSize = 25;
+			break;
+		case "10x10":
+			newGridSize = 10;
+			break;
+		default:
+			break;
+		}
+		
+		this.gridSize = newGridSize;
+		this.clearCanvas();
+	}
+	
+	
+	public void handleMouseEvent(MouseEvent event) {
+		double pressX = event.getX();
+		double pressY = event.getY();
+		
+		int x = (int)(pressX/this.resolution);
+		int y = (int)(pressY/this.resolution);
+		
+		if (this.timeline.getStatus() == Status.STOPPED) {
+			this.grid.getGrid()[x][y].updateCell(true);
+			fillGrid();
+		}
+	}
+	
 	
 	public void changeSpeed(ActionEvent event) {
 		Button button = (Button) event.getSource();
@@ -59,13 +142,13 @@ public class AppController {
 			this.animationSpeed = 0.05;
 			break;
 		case "Normal":
-			this.animationSpeed = 0.5;
+			this.animationSpeed = 0.1;
 			break;
 		case "Slow":
-			this.animationSpeed = 1.0;
+			this.animationSpeed = 0.5;
 			break;
 		default:
-			this.animationSpeed = 1.0;
+			this.animationSpeed = 0.1;
 			break;
 		}
 		
@@ -84,17 +167,15 @@ public class AppController {
 	private void fillGrid() {
         GraphicsContext gc = this.canvas.getGraphicsContext2D();
         gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        
-        double res = canvasSize/this.gridSize;
-
+   
         for (int i = 0; i < this.gridSize; i++) {
             for (int j = 0; j < this.gridSize; j++) {
         		gc.setFill(this.grid.getGrid()[i][j].getStatus() ? Color.WHITE : Color.BLACK);
         		gc.setStroke(Color.GRAY);
         		gc.setLineWidth(1);
         		
-                gc.strokeRect(0.5 + i * res, 0.5 + j * res, res - 1, res - 1);
-        		gc.fillRect(0.5 + i * res, 0.5 + j * res, res - 1, res - 1);
+                gc.strokeRect(0.5 + i * this.resolution, 0.5 + j * this.resolution, this.resolution - 1, this.resolution - 1);
+        		gc.fillRect(0.5 + i * this.resolution, 0.5 + j * this.resolution, this.resolution - 1, this.resolution - 1);
 
             }
           }
